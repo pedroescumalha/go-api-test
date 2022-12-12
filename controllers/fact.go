4738e69b61c8a0b907081af12bb8a0563101e023
Pedro Simões <pedrosimoes93@gmail.com>
2022-12-12 20:24:20 +0000

    fix error condition in fact controller

--- controllers/fact.go
+++ controllers/fact.go
@@ -15,13 +15,13 @@ type Fact struct {
 func GetFact(context *gin.Context) {
 	resp, err := clients.GetChuckNorrisFact()
 
-	var fact Fact = Fact{
-		Message: resp.Value,
-	}
-
-	if err != nil {
+	if err != nil || resp.Value == "" {
 		context.IndentedJSON(http.StatusInternalServerError, "error fetching fact")
 		return
+	}
+
+	var fact Fact = Fact{
+		Message: resp.Value,
 	}
 
 	context.IndentedJSON(http.StatusOK, fact)
