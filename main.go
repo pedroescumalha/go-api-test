fc7ae469b53d955acdbe8e91d503d136fa492b77
Pedro Simões <pedrosimoes93@gmail.com>
2022-12-12 20:18:37 +0000

    add get method in API

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"api/example/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.GET("/fact", controllers.GetFact)
+
 	router.Run("localhost:9090")
 }
